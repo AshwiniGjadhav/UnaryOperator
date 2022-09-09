e3d16e3a4d789a3ba6bb5488ccb440b688b6da95
Dell <Dell@DESKTOP-LGPMCGR>
2022-09-09 16:46:52 +0530

    Add project files.

---
+++ UnaryOperator/Class2.cs
@@ -0,0 +1,20 @@
+﻿/* program for ternary operater */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnaryOperator
+{
+    internal class Class2
+    {
+       // static void Main(string[] args)
+       // {
+        //    int a = 15, b = 16;
+        //    int c = (a > b ? a : b);
+        //    Console.WriteLine(c);
+       // }
+    }
+}
